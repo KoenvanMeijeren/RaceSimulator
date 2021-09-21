8a506df005aa61e8c472895437730e340d0430eb
Koen van Meijeren <52493874+KoenvanMeijeren@users.noreply.github.com>
2021-09-21 08:42:18 +0200

    Add model tests

---
+++ ModelTests/Model_CarShould.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Model;
+using NUnit.Framework;
+
+namespace ModelTests
+{
+    [TestFixture]
+    class Model_CarShould
+    {
+
+        [Test]
+        public void Car_CanCreate()
+        {
+            Car car = new Car(100, 100, 200);
+
+            Assert.AreEqual(100, car.Quality);
+            Assert.AreEqual(100, car.Performance);
+            Assert.AreEqual(200, car.Speed);
+            Assert.IsFalse(car.IsBroken);
+        }
+        
+        [Test]
+        public void Car_CannotCreate_QualityValueOutOfRange()
+        {
+            Assert.Throws<ArgumentOutOfRangeException>(() => new Car(IEquipment.MaximumQuality + 1, IEquipment.MaximumPerformance, IEquipment.MaximumSpeed));
+            Assert.Throws<ArgumentOutOfRangeException>(() => new Car(IEquipment.MinimumQuality - 1, IEquipment.MaximumPerformance, IEquipment.MaximumSpeed));
+            Assert.DoesNotThrow(() => new Car(IEquipment.MaximumQuality, IEquipment.MaximumPerformance, IEquipment.MinimumSpeed));
+            Assert.DoesNotThrow(() => new Car(IEquipment.MaximumQuality, IEquipment.MaximumPerformance, IEquipment.MaximumSpeed));
+            Assert.DoesNotThrow(() => new Car(IEquipment.MaximumQuality - 40, IEquipment.MaximumPerformance, IEquipment.MaximumSpeed));
+        }
+
+        [Test]
+        public void Car_CannotCreate_PerformanceValueOutOfRange()
+        {
+            Assert.Throws<ArgumentOutOfRangeException>(() => new Car(IEquipment.MaximumQuality, IEquipment.MaximumPerformance + 1, IEquipment.MaximumSpeed));
+            Assert.Throws<ArgumentOutOfRangeException>(() => new Car(IEquipment.MaximumQuality, IEquipment.MinimumPerformance - 1, IEquipment.MinimumSpeed));
+            Assert.DoesNotThrow(() => new Car(IEquipment.MaximumQuality, IEquipment.MaximumPerformance, IEquipment.MinimumSpeed));
+            Assert.DoesNotThrow(() => new Car(IEquipment.MaximumQuality, IEquipment.MaximumPerformance, IEquipment.MaximumSpeed));
+            Assert.DoesNotThrow(() => new Car(IEquipment.MaximumQuality, IEquipment.MaximumPerformance - 40, IEquipment.MaximumSpeed));
+        }
+
+        [Test]
+        public void Car_CannotCreate_SpeedValueOutOfRange()
+        {
+            Assert.Throws<ArgumentOutOfRangeException>(() => new Car(IEquipment.MaximumQuality, IEquipment.MaximumPerformance, IEquipment.MaximumSpeed + 1));
+            Assert.Throws<ArgumentOutOfRangeException>(() => new Car(IEquipment.MaximumQuality, IEquipment.MaximumPerformance, IEquipment.MinimumSpeed - 1));
+            Assert.DoesNotThrow(() => new Car(IEquipment.MaximumQuality, IEquipment.MaximumPerformance, IEquipment.MinimumSpeed));
+            Assert.DoesNotThrow(() => new Car(IEquipment.MaximumQuality, IEquipment.MaximumPerformance, IEquipment.MaximumSpeed));
+            Assert.DoesNotThrow(() => new Car(IEquipment.MaximumQuality, IEquipment.MaximumPerformance, IEquipment.MaximumSpeed - 40));
+        }
+
+        [Test]
+        public void Car_CanCreate_CanSetRandomQuality()
+        {
+            Car car = new Car(IEquipment.MaximumQuality, IEquipment.MaximumPerformance, IEquipment.MaximumSpeed);
+
+            int previousQuality = car.Quality;
+            car.SetRandomQuality();
+            Assert.AreNotSame(car.Quality, previousQuality);
+            Assert.IsTrue(car.Quality >= IEquipment.MinimumQuality);
+            Assert.IsTrue(car.Quality <= IEquipment.MaximumQuality);
+        }
+
+        [Test]
+        public void Car_CanCreate_CanSetRandomPerformance()
+        {
+            Car car = new Car(IEquipment.MaximumQuality, IEquipment.MaximumPerformance, IEquipment.MaximumSpeed);
+
+            int previousPerformance = car.Performance;
+            car.SetRandomPerformance();
+            Assert.AreNotSame(car.Performance, previousPerformance);
+            Assert.IsTrue(car.Performance >= IEquipment.MinimumPerformance);
+            Assert.IsTrue(car.Performance <= IEquipment.MaximumPerformance);
+        }
+
+    }
+}
---
+++ ModelTests/Model_SectionShould.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Model;
+using NUnit.Framework;
+using NUnit.Framework.Internal;
+
+namespace ModelTests
+{
+    [TestFixture]
+    class Model_SectionShould
+    {
+
+        [Test]
+        public void CanCreateSection()
+        {
+            Section sectionOne = new Section(SectionTypes.StartGrid);
+            Section sectionTwo = new Section(SectionTypes.LeftCorner);
+            Section sectionThree = new Section(SectionTypes.Straight);
+            Section sectionFour = new Section(SectionTypes.RightCorner);
+            Section sectionFive = new Section(SectionTypes.Finish);
+
+            Assert.AreEqual(SectionTypes.StartGrid, sectionOne.SectionType);
+            Assert.AreEqual(SectionTypes.LeftCorner, sectionTwo.SectionType);
+            Assert.AreEqual(SectionTypes.Straight, sectionThree.SectionType);
+            Assert.AreEqual(SectionTypes.RightCorner, sectionFour.SectionType);
+            Assert.AreEqual(SectionTypes.Finish, sectionFive.SectionType);
+        }
+
+    }
+}
