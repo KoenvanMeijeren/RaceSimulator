3cdcaa9912fa2dd6a6ca8364c2b7699b152d0d5f
Koen van Meijeren <52493874+KoenvanMeijeren@users.noreply.github.com>
2021-10-07 15:05:22 +0200

    Implement loading images from cache

---
+++ WPFRaceSimulator/WPFImageBuilder.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using System.Drawing;
+
+namespace WPFRaceSimulator
+{
+    /// <summary>
+    /// Provides a class for creating, loading and editing images. E.g. rotating or mirroring images.
+    /// </summary>
+    public static class WPFImageBuilder
+    {
+
+        private static Dictionary<string, Bitmap> CachedImages;
+
+        public static void ClearCache()
+        {
+            WPFImageBuilder.CachedImages.Clear();
+        }
+        
+        public static Bitmap LoadImage(string url)
+        {
+            if (!WPFImageBuilder.CachedImages.ContainsKey(url))
+            {
+                WPFImageBuilder.CachedImages.Add(url, new Bitmap(Image.FromFile(url)));
+            }
+
+            return WPFImageBuilder.CachedImages[url];
+        }
+        
+    }
+}
